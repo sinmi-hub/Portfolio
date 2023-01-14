d44d1f1a5190edc99ed281865d78b875bca661bb
Mosinmiloluwa <64702488+simi-hub@users.noreply.github.com>
2023-01-14 03:44:55 -0500

    Interface that represents price of items in restaurant

--- src/TakeOutSystem/PricedItem.java
+++ src/TakeOutSystem/PricedItem.java
@@ -7,11 +7,14 @@ package TakeOutSystem;
  * they are both needed. It was also created to prevent having to rewrite
  * code if another item different from food is added to takeout system. This
  * interface defines two method to be implemented getPrice(), and setPrice();
- * DRY
+ * DRY. setPrice() was not used in this scope of project, so it was commented
+ * out. There was no avenue where price of an item needed to change yet. If
+ * needed, it can be simply commented out an implemented in necessary class
  *
  * @param <T> (generic class that takes anything number ot its subclasses)
  */
 public interface PricedItem<T extends Number> {
     T getPrice();// returns price of object in which it is implemented
-    void setPrice(T price);// sets price of object in which it is implemented
+
+//    void setPrice(T price);// sets price of object in which it is implemented
 }
